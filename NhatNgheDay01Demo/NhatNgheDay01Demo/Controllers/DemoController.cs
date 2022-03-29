bcf872c2e3205f96c78db19828b0b9fb6e554783
PhamQuocDuy <phamquocduy1110@gmail.com>
2022-03-29 19:29:13 +0700

    - Create Demo Controller

---
+++ NhatNgheDay01Demo/NhatNgheDay01Demo/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NhatNgheDay01Demo.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // /Demo/Hello
+        // /Demo/Hello?ten=NhatNghe // routing default
+
+        [HttpGet("Chao")]
+        [HttpGet("/Hello")]
+        [HttpGet("/Hello/{ten}")]
+        public string Hello(string ten)
+        {
+            return $"Hello {ten}.";
+        }
+
+        // Demo friendly URL/SEO
+        [HttpGet("/{loai}/{hanghoa}")]
+        public string LayHangHoa(string loai, string hanghoa)
+        {
+            return $"Loại: {loai}/hàng hóa: {hanghoa}";
+        }
+    }
+}
