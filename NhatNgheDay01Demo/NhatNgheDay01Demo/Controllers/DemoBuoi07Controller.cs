d4f1bb64607db79e851c734e1d31e984415e9526
PhamQuocDuy <phamquocduy1110@gmail.com>
2022-03-31 19:21:23 +0700

    - Create DemoBuoi07 Controller - Create Index for DemoBuoi07

---
+++ NhatNgheDay01Demo/NhatNgheDay01Demo/Controllers/DemoBuoi07Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NhatNgheDay01Demo.Models;
+
+namespace NhatNgheDay01Demo.Controllers
+{
+    public class Demo07Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Nhất Nghệ";
+            ViewBag.NamThanhLap = new DateTime(2003, 3, 10);
+            return View();
+        }
+    }
+}
