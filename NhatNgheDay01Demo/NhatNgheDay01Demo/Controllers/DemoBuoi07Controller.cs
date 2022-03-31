70ee6dc439787b0dfa1e61b597ebe59f3cc095c8
PhamQuocDuy <phamquocduy1110@gmail.com>
2022-03-31 20:48:00 +0700

    - Create list product page - Add static data

--- NhatNgheDay01Demo/NhatNgheDay01Demo/Controllers/DemoBuoi07Controller.cs
+++ NhatNgheDay01Demo/NhatNgheDay01Demo/Controllers/DemoBuoi07Controller.cs
@@ -9,6 +9,24 @@ namespace NhatNgheDay01Demo.Controllers
         {
             ViewBag.HoTen = "Nhất Nghệ";
             ViewBag.NamThanhLap = new DateTime(2003, 3, 10);
+            ViewBag.HangHoa = new Product
+            {
+                Id = Guid.NewGuid(),
+                ProductName = "Bia Sài Gòn",
+                Price = 14500,
+                Quantity = 24
+            };
+
+            var hangHoa = new Product
+            {
+                Id = Guid.NewGuid(),
+                ProductName = "Bia Sài Gòn",
+                Price = 14500,
+                Quantity = 24
+            };
+            ViewBag.HangHoa = hangHoa;
+            TempData["hanghoa"] = hangHoa;
+
             return View();
         }
     }
---
+++ NhatNgheDay01Demo/NhatNgheDay01Demo/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NhatNgheDay01Demo.Models;
+
+namespace NhatNgheDay01Demo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private List<Product> Products = new List<Product>()
+        {
+            new Product
+            {
+                Id = Guid.NewGuid(),
+                ProductName = "Bia 333",
+                Price = 15900,
+                Quantity = 23
+            },
+            new Product
+            {
+                Id = Guid.NewGuid(),
+                ProductName = "Bia Heineiken",
+                Price = 19500,
+                Quantity = 29
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(Products);
+        }
+    }
+}
