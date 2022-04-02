1d9e8b161f6ed981b9f3c8ad73dc0db401e26235
PhamQuocDuy <phamquocduy1110@gmail.com>
2022-04-02 19:05:40 +0700

    - Create controller student - Create model student

---
+++ NhatNgheDay01Demo/NhatNgheDay01Demo/Controllers/StudentController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NhatNgheDay01Demo.Controllers
+{
+    public class StudentController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NhatNgheDay01Demo/NhatNgheDay01Demo/Models/StudentInfo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace NhatNgheDay01Demo.Models
+{
+    public class StudentInfo
+    {
+        [Display(Name = "Mã sinh viên")]
+        [Required(ErrorMessage = "*")]
+        public string StudentId { get; set; }
+
+        [Display(Name = "Họ tên sinh viên")]
+        [Required(ErrorMessage = "*")]
+        [MinLength(5, ErrorMessage = "Tên sinh viên phải tối thiểu 5 ký tự")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Điểm trung bình")]
+        [Range(0, 10, ErrorMessage = "Điểm trung bình phải từ 0 đến 10")]
+        public double Grade { get; set; }
+
+        [Display(Name = "Xếp loại")]
+        public string Rating { get; set; }
+    }
+}
