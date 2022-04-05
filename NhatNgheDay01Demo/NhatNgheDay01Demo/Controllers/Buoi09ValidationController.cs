7abb0b4b9b8af01745ee85a0339aaf1d0b2e16e5
PhamQuocDuy <phamquocduy1110@gmail.com>
2022-04-05 20:09:20 +0700

    - Create Buoi09ValidationController - Create DemoRazor view demo

---
+++ NhatNgheDay01Demo/NhatNgheDay01Demo/Controllers/Buoi09ValidationController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using NhatNgheDay01Demo.Models;
+
+namespace NhatNgheDay01Demo.Controllers
+{
+    public class Buoi09ValidationController : Controller
+    {
+        public IActionResult DemoAsyncRun()
+        {
+            Stopwatch sw = new Stopwatch();
+            Demo demo = new Demo();
+            sw.Start();
+            demo.Test01();
+            demo.Test02();
+            demo.Test03();
+            // Task.WaitAll()
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms.");
+
+        }
+
+        public async Task<IActionResult> Async()
+        {
+            Stopwatch sw = new Stopwatch();
+            Demo demo = new Demo();
+            sw.Start();
+            var a = demo.Test01Async();
+            var b = demo.Test02Async();
+            var c = demo.Test03Async();
+            await a; await b; await c;
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds / 1000} ms.");
+        }
+
+        public IActionResult DemoRazor()
+        {
+            return View();
+        }
+    }
+}
---
+++ NhatNgheDay01Demo/NhatNgheDay01Demo/Models/Demo.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+
+namespace NhatNgheDay01Demo.Models
+{
+    public class Demo
+    {
+        public string Test01()
+        {
+            Thread.Sleep(2000);
+            return "Test01 - 2s";
+        }
+
+        public int Test02()
+        {
+            Thread.Sleep(5000);
+            return 123;
+        }
+        public void Test03()
+        {
+            Thread.Sleep(3000);
+        }
+
+        public async Task<int> Test01Async()
+        {
+            await Task.Delay(5000);
+            return 7777;
+        }
+        public async Task<string> Test02Async()
+        {
+            await Task.Delay(2000);
+            return "Nhất Nghệ";
+        }
+        public async Task Test03Async()
+        {
+            await Task.Delay(3000);
+        }
+    }
+}
