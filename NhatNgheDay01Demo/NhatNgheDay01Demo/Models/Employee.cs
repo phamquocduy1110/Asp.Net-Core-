7d8f802026380706d1857df6540b3807392281e1
PhamQuocDuy <phamquocduy1110@gmail.com>
2022-04-05 21:01:02 +0700

    - Create new Employee model

---
+++ NhatNgheDay01Demo/NhatNgheDay01Demo/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NhatNgheDay01Demo.Models
+{
+    public class Employee
+    {
+        [Display(Name = "Mã định danh")]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Mã nhân viên")]
+        [RegularExpression(@"NV\d{5}", ErrorMessage = "Đúng định dạng NVxxxxx")]
+        public string? EmployeeId { get; set; }
+
+        public string? FullName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [DataType(DataType.Url)]
+        public string? Website { get; set; }
+    }
+}
