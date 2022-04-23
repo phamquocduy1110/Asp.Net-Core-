1f281d53edcc41eac82c8f158e37a1daa4a0049b
PhamQuocDuy <phamquocduy1110@gmail.com>
2022-04-23 19:04:28 +0700

    - Add new database connection - Create new ADONET controller

---
+++ NhatNgheDay01Demo/NhatNgheDay01Demo/Controllers/ADONETController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace NhatNgheDay01Demo.Controllers
+{
+    public class ADONETController1 : Controller
+    {
+
+        const string ChuoiKetNoi = @"Data Source=DESKTOP-2TJNS3V\MSSQL2019;Initial Catalog=eStore20;Integrated Security=True";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DocDatabase()
+        {
+            var sql = "SELECT * FROM Loai ORDER BY TenLoai";
+
+            SqlConnection
+        }
+    }
+}
